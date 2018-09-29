1163b9969b9e4ec3d5118a931301fa5cf5fedd3b
helio <helio_dantas@outlook.com>
2018-09-29 01:59:10 -0300

    1.0.1

---
+++ cadastrar_cliente.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>cadastrar_Paciente</title>
+</head>
+<body>
+<h1>cadastrar_Paciente</h1>
+
+<form action="adicionar_cliente.php" method= "post">
+<label for="nome">nome do Paciente</label>
+<input name = "nome" type="text" id = "nome" >
+<label for="cpf">cpf do Paciente</label>
+<input  name = "cpf" type="text" id = "cpf">
+<input type="submit" values = "salvar">
+</form>
+
+
+
+</body>
+</html>
