0a99f01934c7eb66adf736fb08a4f0eb170f34d9
HelioOliveira <helio_dantas@outlook.com>
2018-10-11 09:06:17 -0300

    titulos das telas principais adcionadas

--- sistema.php
+++ sistema.php
@@ -28,10 +28,10 @@ if (obterSessao('usuario_matricula') === null || !isset($_COOKIE["usuario_logado
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <a class="navbar-brand" href="index.php"><h2>SisCon</h2></a>
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
