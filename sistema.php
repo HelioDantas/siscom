46866d43e79b831ee6b092f9a2361fe2dbca51fd
HelioOliveira <helio_dantas@outlook.com>
2018-10-16 13:09:24 -0300

    navbar com opções

--- sistema.php
+++ sistema.php
@@ -18,7 +18,12 @@ if (obterSessao('usuario_matricula') === null || !isset($_COOKIE["usuario_logado
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/estilo.css">
     <link rel="stylesheet"  href="css/bootstrap.css" />
-    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     
     <style> p{color:white; } span{color :black } </style>    
     
     <style> a{ color: #2BBBAD;}</style>
@@ -54,6 +59,34 @@ if (obterSessao('usuario_matricula') === null || !isset($_COOKIE["usuario_logado
                     <a class = "nav-link layat" href="sair.php">Sair</a>
             </div>
           </nav>
+          <ul class="nav nav-tabs white">
+    <li class="nav-item dropdown">
+   <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cadastro</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Pacientes</a>
+          <a class="dropdown-item" href="#">Funcionários</a>
+         <a class="dropdown-item" href="#">Convênios</a>
+           <a class="dropdown-item" href="#">Procedimentos</a>
+           <a class="dropdown-item" href="#">Laboratórios</a>
+      </li>
+
+       <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Agenda</a>
+             <div class="dropdown-menu">
+           <a class="dropdown-item" href="#">Pacientes</a>
+           <a class="dropdown-item" href="#">Eventos</a>
+</li>
+  <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Registro Clínico</a>
+</li>
+  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Relatórios</a>
+         <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Valores</a>
+         <a class="dropdown-item" href="#">Convênios</a>
+          <a class="dropdown-item" href="#">Eventos</a>
+</li>
+</ul>
 
 
 <script src="js/bootstrap.min.js"></script>
