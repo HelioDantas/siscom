7985ad933b2cbc5857ed2f35ce7102b70513b69f
rafaelAlvg <rafaelAlvarengadev@outlook.com>
2018-10-30 17:08:46 -0300

    começando auth login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -15,6 +16,8 @@ class UserController extends Controller
     function ehUsuarioValido()
     {
 
+        if (Auth::check()) {
         
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -84,3 +84,7 @@ Route::prefix('pacientes')->group(function () {
 
 
 //Route::resource('/pacientes', 'PacienteController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
