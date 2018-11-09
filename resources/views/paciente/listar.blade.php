cc58a0ce3738d46606656a7ffd21aeec55534507
rafaelAlvg <rafaelAlvarengadev@outlook.com>
2018-11-09 11:35:52 -0200

    ok

--- resources/views/paciente/listar.blade.php
+++ resources/views/paciente/listar.blade.php
@@ -27,7 +27,7 @@ TESTANDO: <span style="color:red;">visualização e paginação do dados do paci
 
 @section('tela')
     
-<div class="card text-center">
+<div class="card text-center mb-3">
     <div class="card-header">
             <h3 class="titulopacientes">Pacientes Cadastrados</h3>
     </div>
@@ -35,16 +35,16 @@ TESTANDO: <span style="color:red;">visualização e paginação do dados do paci
             <table class="table table-hover">
             <thead class="thead-dark">
               <tr>
-                <th s>prontuario      </th>
+                <th >prontuario      </th>
                 <th >nome            </th>
                 <th >cpf             </th>
                 <th >identidade      </th>
-                <th >dataDeNascimento</th>
+                <th >Nascimento</th>
                 <th >sexo            </th>
-                <th >etnia           </th>
+     {{--       <th >etnia           </th>      --}}
                 <th >nacionalidade   </th>
-                <th >naturalidade    </th>
-                <th >escolaridade    </th>
+    {{--        <th >naturalidade    </th>   
+                <th >escolaridade    </th>--}}
     <!--        <th scope="col">rua             </th>
                 <th scope="col">numero          </th>
                 <th scope="col">bairro          </th>
@@ -52,10 +52,10 @@ TESTANDO: <span style="color:red;">visualização e paginação do dados do paci
                 <th scope="col">cidade          </th>
                 <th scope="col">estado          </th>
                 <th scope="col">telefone        </th>
-                <th scope="col">celular         </th>
-                <th scope="col">email           </th>
-                <th scope="col">profissao       </th>
-                <th scope="col">status_2        </th>   -->
+                <th scope="col">celular         </th>-->
+                <th scope="col">email           </th> 
+       {{--          <th scope="col">profissao       </th> --}}
+                <th scope="col">status        </th>  
                 <th scope="col">opções    </th>
               </tr>
             </thead>
@@ -70,10 +70,10 @@ TESTANDO: <span style="color:red;">visualização e paginação do dados do paci
                  <td>       {{$p->identidade}}          </td>
                  <td>       {{$p->dataDeNascimento}}    </td>
                  <td>       {{$p->sexo}}                </td>
-                 <td>       {{$p->etnia}}               </td>
+  {{--            <td>       {{$p->etnia}}               </td>   --}}
                  <td>       {{$p->nacionalidade}}       </td>
-                 <td>       {{$p->naturalidade}}        </td>
-                 <td>       {{$p->escolaridade}}        </td>
+    {{--              <td>       {{$p->naturalidade}}        </td> 
+                 <td>       {{$p->escolaridade}}        </td>  --}}
     <!--         <td>       {{$p->rua}}                 </td>
                  <td>       {{$p->numero}}              </td>
                  <td>       {{$p->bairro}}              </td>
@@ -81,10 +81,10 @@ TESTANDO: <span style="color:red;">visualização e paginação do dados do paci
                  <td>       {{$p->cidade}}              </td>
                  <td>       {{$p->estado}}              </td>
                  <td>       {{$p->telefone}}            </td>
-                 <td>       {{$p->celular}}             </td>
-                 <td>       {{$p->email}}               </td>
-                 <td>       {{$p->profissao}}           </td>
-                 <td>       {{$p->status_2}}            </td>  -->
+                 <td>       {{$p->celular}}             </td>-->
+                 <td>       {{$p->email}}               </td> 
+      {{--           <td>       {{$p->profissao}}           </td>  --}}
+                 <td>       {{$p->status}}            </td>  
                 <td>
                     <a href="editar/{{$p->id}}"><i class="fas fa-edit"></i></a> 
                 
