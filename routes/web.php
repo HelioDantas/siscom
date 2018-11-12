31fb36294452c11c15f95f1e1b921e04cfd80a0f
HelioOliveira <helio_dantas@outlook.com>
2018-11-12 12:32:26 -0200

    integração convenio3

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ Route::post("/login", ['as' => 'user.login', 'uses' => 'LoginController@login'])
  */
 
 
-Route::prefix('pacientes')->group(function () { //->middleware('Autorizador')-
+Route::prefix('pacientes')->middleware('Autorizador')->group(function () { //->middleware('Autorizador')-
   Route::get('listar'       , 'PacienteController@listar')->name(   'paciente.listar' );
   Route::get('novo'         , 'PacienteController@novo')->name(     'paciente.novo'   );
   Route::post('create'      , 'PacienteController@create')->name(   'paciente.create' );
@@ -99,7 +99,7 @@ Route::prefix('pacientes')->group(function () { //->middleware('Autorizador')-
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::prefix('funcionario')->group(function () { //->middleware('Autorizador')->
+Route::prefix('funcionario')->middleware('Autorizador')->group(function () { //->middleware('Autorizador')->
 
   Route::get('cad'            , 'FuncionarioController@novo')->name(        'funcionario.novo'      );
   Route::get('listar'         , 'FuncionarioController@listar')->name(      'funcionario.listar'    );
