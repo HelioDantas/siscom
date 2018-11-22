0e8e917bc1cbb0ce4b4a02a1116c7f179cd56c04
Nadia <nadiaaraujo1@gmail.com>
2018-11-22 18:05:55 -0200

    convenio atualizacao2

--- resources/views/convenio/index.blade.php
+++ resources/views/convenio/index.blade.php
@@ -4,12 +4,64 @@
                 <meta charset ="utf-8">
                 <meta http-equi="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-               
-
+               <link href="{{asset('css/app.css')}}" rel="stylesheet">
                 <title>Paginação</title>
+                 <style>
+                    body{
+                        padding:20 px; 
+                    }
+                </style>
             </head>
         </body>
-    <div class = "container"></div>
+    <div class = "container">
+        <div class="card text-center"></div>
+            <div class="card-header"></div> Tabela de Convênios</div>
+            <div class="card-body">
+                <table class="table table-hover">
+                        <thead>
+                            <th scope="col">CNPJ</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Adesão</th>
+                            <th scope="col">Banco</th>
+                            <th scope="col">Agencia</th>
+                            <th scope="col">Conta</th>
+                            <th scope="col">Status</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ASSIM
+                            </tr>
+                            <tr>
+                                <td>UNIMED
+                            </tr>
+                            <tr>
+                                <td>BRADESCO
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>SULAMERICA
+                            </tr>
+                            <tr>
+                                <td>INTERMEDICA
+                            </tr>
+                            <tr>
+                                <td>PETROBRAS
+                            </tr>
+                            <tr>
+                                <td>AMIL
+                            </tr>
+                            <tr>
+                                <td>CAIXA
+                            </tr>
+                            <tr>
+                                <td>BNDES
+                            </tr>
+                            <tr>
+                                BANCO DO BRASIL 
+                                </td>
+                           </tr>
+                        </tbody>
+
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>
