3f39340ff2174fa98210b249127e10a418dc9a2e
HelioOliveira <helio_dantas@outlook.com>
2018-11-29 12:23:12 -0200

    formulario letra minuscula

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -17,7 +17,7 @@ class PermissionController extends Controller
             if(!$request->session()->get("user")->permission()->where('permissao_id', 1)->get()->isEmpty())
                 return true;
      
-              abort(403,'Unauthorized');
+              abort(403,'Não autozizado');
 
 
 
@@ -30,7 +30,7 @@ class PermissionController extends Controller
             if( !$request->session()->get("user")->permission()->where('permissao_id', 3)->get()->isEmpty())
                 return true;
      
-              abort(403,'Unauthorized');
+              abort(403,'Não autorizado');
 
     }
 
@@ -41,7 +41,7 @@ class PermissionController extends Controller
             if( (!$request->session()->get("user")->permission()->where('permissao_id', 4)->get()->isEmpty()))
                 return true;
      
-              abort(403,'Unauthorized');
+              abort(403,'Não autorizado');
 
          }
 
@@ -53,7 +53,7 @@ class PermissionController extends Controller
             if( (!$request->session()->get("user")->permission()->where('permissao_id', 5)->get()->isEmpty()))
                 return true;
      
-              abort(403,'Unauthorized');
+              abort(403,'Não autorizado');
 
          }
 
@@ -64,7 +64,68 @@ class PermissionController extends Controller
             if( (!$request->session()->get("user")->permission()->where('permissao_id', 6)->get()->isEmpty()))
                 return true;
      
-              abort(403,'Unauthorized');
+              abort(403,'Não autorizado');
+
+            }
+
+
+
+
+               public static function pedit(Request $request)
+    {
+        //  form para editar infos de um funcionario
+  
+            if(!$request->session()->get("user")->permission()->where('permissao_id', 7)->get()->isEmpty())
+                return true;
+     
+              abort(403,'Não autorizado');
+
+
+
+    }
+
+       public static function pnovo(Request $request)
+        {
+        //  form para editar infos de um funcionario
+  
+            if( !$request->session()->get("user")->permission()->where('permissao_id', 8)->get()->isEmpty())
+                return true;
+     
+              abort(403,'Não autorizado');
+
+    }
+
+     public static function pcreate(Request $request)
+        {
+        //  form para editar infos de um funcionario
+  
+            if( (!$request->session()->get("user")->permission()->where('permissao_id', 9)->get()->isEmpty()))
+                return true;
+     
+              abort(403,'Não autorizado');
+
+         }
+
+
+         public static function pshow(Request $request)
+        {
+        //  form para editar infos de um funcionario
+  
+            if( (!$request->session()->get("user")->permission()->where('permissao_id', 10)->get()->isEmpty()))
+                return true;
+     
+              abort(403,'Não autorizado');
+
+         }
+
+             public static function pdestroy(Request $request)
+            {
+        //  form para editar infos de um funcionario
+  
+            if( (!$request->session()->get("user")->permission()->where('permissao_id', 11)->get()->isEmpty()))
+                return true;
+     
+              abort(403,'Não autorizado');
 
             }
 
