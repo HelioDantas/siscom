ba5e7530a70fbed406099038d1000b0fa6781732
rafaelAlvg <rafaelAlvarengadev@outlook.com>
2018-11-29 09:16:28 -0200

    historico de convenios/plnao ok

--- resources/views/paciente/show.blade.php
+++ resources/views/paciente/show.blade.php
@@ -577,7 +577,7 @@
 </fieldset><!--endereço-->
 
 
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Planos Do paciente</button>
 
 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-lg">
