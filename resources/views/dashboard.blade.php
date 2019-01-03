72c423f51bb7e669a7da7c92a8f888f2bb4c0881
HelioOliveira <helio_dantas@outlook.com>
2019-01-03 07:58:04 -0200

    Vião do Medico

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,7 +38,7 @@
                           <h5 class="card-title">Agenda</h5>
                           <p class="card-text">histórico de agendamentos e agendamentos do dia </p>
                           @if(Auth::user()->funcionario->medico)
-                           <a href="{{ route('medico.agenda'') }}" class="btn btn-primary">Consultar</a>
+                           <a href="{{ route('medico.agenda') }}" class="btn btn-primary">Consultar</a>
                            @else
                           <a href="{{ route('agenda.index') }}" class="btn btn-primary">Consultar</a>
                           @endif
